657e98eed8271f020cdf80b96e7d0f9402612430
gpratyu <pratyushagovindaraju@gmail.com>
2020-01-14 16:32:30 -0500

    Functioning Word Cloud

--- scripts/dictionary.py
+++ scripts/dictionary.py
@@ -1,13 +1,20 @@
 import os
 import csv
-                 
+import json 
                
-with open(words.csv, 'r') as csvfile: 
+with open('words.csv', 'r') as csvfile: 
 
 	dictReader = csv.DictReader(csvfile)
-	for row in dictReader:
 
-		print(row)
-		
+	
+	rows = list(dictReader)
+	rows = [dict(row) for row in rows]
+	rows = [{"text": row['text'], "size": int(row['size'])} for row in rows]
+
+#	print(rows)
+#	// for row in rows:
+#
+#		// print(row)
+print(json.dumps(rows))		
  
  
