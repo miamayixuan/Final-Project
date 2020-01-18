6b3513ada795108f10ed0ce331b5134e8c680dd8
miamayixuan <mayixuanli@yahoo.com>
2020-01-18 18:47:58 -0500

    add source to tab4

--- scripts/tab4.js
+++ scripts/tab4.js
@@ -1,7 +1,7 @@
 function drawBar(datafile, cssselector, colorrange) {
 
 var svg = d3.select(cssselector),
-    margin = {top: 50, right: 20, bottom: 30, left: 150},
+    margin = {top: 50, right: 20, bottom: 0, left: 150},
     width = +svg.attr("width") - margin.left - margin.right,
     height = +svg.attr("height") - margin.top - margin.bottom,
     g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
